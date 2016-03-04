306f5ede2431d0b2b5406d58ff0473aae6e15ce5
Tato Moaki <moakitato@live.com>
2016-03-04 22:17:01 +0200

    Added the main driver file that passes command line parameters

---
+++ volimage.cpp
@@ -0,0 +1,15 @@
+int main(int argc, char* argv[]) {
+	if(argc == 6){
+	std::cout << "Compute difference map"<<std::endl;}
+	else if (argc == 5) {
+	std::cout << "Extract and write to outputfile"std::endl;}
+	else {
+	std::cout << "Number of Images" << std::endl;
+	std::cout << "Number of bytes required" << std::endl;
+}
+	 
+
+
+
+return 0;
+}
