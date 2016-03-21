f53478eeffa0209db2286c31e095773a50a5c359
Tato Moaki <moakitato@live.com>
2016-03-22 00:58:47 +0200

    Implemented difference map. When diff is computed on the same image, a blank greyscale. I think the method thats what is expected ./volimage ./raws/MRI -d 4 4 output

--- VolImage.cpp
+++ VolImage.cpp
@@ -3,42 +3,103 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-
+#include <cmath>
 //Volimage Method definitions
 VolImage::VolImage() {
 	}
 VolImage::~VolImage() {
-	
+	for(unsigned int i = 0; i < slices.size(); i++) {
+		for(int j = 0; j < height; j++) {
+			delete[] slices[i][j];			
+			}
+			delete []slices[i];	
+		}	
 	}
 
 bool VolImage::readImages(std::string baseName){ 
 	using namespace std;
-	baseName += ".data";
+	string *file_name = &baseName;	
+	//streampos size;
 	int noImages;
 	string line;
-	ifstream ifs (baseName.c_str(), ios::in|ios::binary);
+	unsigned char **array = NULL;
+	ifstream ifs ((baseName+".data").c_str(), ios::in);
 	
 	if(ifs.is_open()) {
 		while(getline(ifs, line)){
-			istringstream iss(line);
-			for(int i = 0; i < 3; i++) {
-				if(i == 0)iss >> width;
-				if(i == 1)iss >> height;
-				iss	>> noImages;
-				}
+			istringstream iss(line);			
+				iss >> width;
+				iss >> height;
+				iss	>> noImages;				
+		ifs.close();	
+		}				
+		for (int i = 0; i < noImages; ++i) {
+			
+			ostringstream ss;
+			ss << i;
+			string file_ = *(file_name) + ss.str() + ".raw";
+			//cout << file_ << endl;
+			ifstream rfs(file_.c_str(), ios::in|ios::binary);
+			
+			if(rfs.is_open()) {
+					array = new unsigned char*[height];	
+					/**				
+					for(int i = 0; i < height; i++) {
+						array[i] = new unsigned char[width];						
+ 					}**/
+ 					unsigned char* array_;
+ 					//rfs.seekg(0, ios::beg); 					
+ 					for (int i = 0;i < height; i++) {
+						array_ = new unsigned char[width];
+						//rfs.read((unsigned char*)(&array[i),width);
+						rfs.read((char*)array_, width);	
+						array[i] = array_;					
+					}
+ 					slices.push_back(array);
+					rfs.close();				
+				}							
+			}
+			//cout << slices.size();
+			return true;				
 		}
-		
-		ifs.close();
-		return true;
-		}
-	
+	cerr << "Header File not opened" << endl;
 	return false;
-	}
+}
 void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix) {
+	using namespace std;
+	string file = output_prefix+".raw";
+	ofstream ofs(file.c_str(), ios::out|ios::binary);
+	if(ofs.is_open()) {
+	for(int i = 0; i < height; i++) {
+		for(int j = 0; j < width; j++) {
+				char diff = abs(((float)slices[sliceI][i][j]- (float)slices[sliceJ][i][j])/2);
+				ofs.write(&diff, 1);	
+							
+			}		
+		}
+		}
 	
 	}
 void VolImage::extract(int sliceId, std::string output_prefix) {
+	using namespace std;
+	
+	string file = output_prefix+".raw";
+	ofstream ofs ((output_prefix+".data").c_str(), ios::out); 
+	if(ofs.is_open()) {
+		ofs << width << " " << height << " 1"; 
+		ofs.close();
 	}
+	ofstream raws((file).c_str(), ios::out|ios::binary);
+	if (raws.is_open()){
+	for(int i = 0; i < height; i++) {
+		char* raw = (char*)slices[sliceId][i];
+		raws.write(raw,width);			
+		}
+		raws.close();
+	}
+		
+	}
+	
 int VolImage::volImageSize(void){
 	
 	return 1;
--- volimage.cpp
+++ volimage.cpp
@@ -4,20 +4,28 @@
 #include "VolImage.h"
 
 int main(int argc, char* argv[]) {
+	using namespace std;
 	std::string imageBase = argv[1];
-	VolImage vImage;
-	bool flag = vImage.readImages(imageBase);
+	VolImage voliImage;						//VoliImage Object
+	bool flag = voliImage.readImages(imageBase);
+	
 	
 	if (flag) {	
-		if(argc == 5) {
-			std::cout << "Extract Image" << std::endl;	
+		//Extract Image
+		if(argc == 5) {				
 			int idxSlice;
-			std::istringstream (argv[3]) >> idxSlice;		
-			std::cout << idxSlice << std::endl;
+			std::istringstream (argv[3]) >> idxSlice;
+			string output_file_name = argv[4];					
+			voliImage.extract(idxSlice, output_file_name);
 			
 		}
+		//Compute Difference Map
 		else if (argc == 6) {
-			std::cout << "Compute Difference Map" << std::endl;	 		
+			int i, j;
+			std::istringstream (argv[3]) >> i;
+			std::istringstream (argv[4]) >> j;
+			string output_file_name = argv[5];
+			voliImage.diffmap(i, j, output_file_name); 		
 	}
 	else {
 		std::cout << "Build internal representation" << std::endl;
@@ -25,8 +33,7 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	else {
-		std::cerr << "Prefix for file sequence returned error!" << std::endl; 
-		
+		std::cerr << "Prefix for file sequence returned error!" << std::endl; 		
 		}
 return 0;
 }
